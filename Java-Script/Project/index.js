385599814ff44a7110d49c1699ca8027af01928a
shaanif ahmed <shaaniffakki@gmail.com>
2025-07-24 11:54:29 +0000

    crted project main logic described to crete it

---
+++ Java-Script/Project/index.js
@@ -0,0 +1,15 @@
+let arr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+let a = Math.floor(Math.random () * arr.length)
+// console.log(arr[a]);
+
+let arr2 = ['~','@','#','$','%','^','&','*','(',')','_','+','=','{','}']
+let b = Math.floor(Math.random () * arr2.length)
+// console.log(arr2[b]);
+
+let arr3 = ['0','1','2','3','4','5','6','7','8','9']
+let c = Math.floor(Math.random () * arr3.length)
+//console.log(arr3[c]);
+
+let arr4 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let d = Math.floor(Math.random () * arr4.length)
+console.log(arr4[d]);
