5d01026d148417302ea151c0fd606ce2670cd40e
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-08 09:52:03 +0530

    done with designing and siome logical part

--- ReactJS/project-18/src/components/TodoList.jsx
+++ ReactJS/project-18/src/components/TodoList.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-const TodoList = ({tasks}) => {
+import React from "react";
+
+const TodoList = ({ tasks }) => {
   return (
-     <div className="mt-4 text-center">
+    <div className="mt-4 text-center space-y-2 flex flex-col justify-center items-center">
       {tasks.length === 0 ? (
-        <p>No tasks yet.</p>
+        <p className="text-gray-400">No tasks yet.</p>
       ) : (
         tasks.map((t, index) => (
-          <p key={index}>Task {index + 1}: {t}</p>
+         <div
+            key={index}
+            className="flex items-center justify-between gap-3 bg-gradient-to-r from-purple-500 to-pink-500 p-3 rounded-lg shadow-md w-96"
+          >
+            <input
+              type="checkbox"
+              className="w-5 h-5 accent-purple-700"
+              onChange={(e) =>
+                e.target.nextSibling.classList.toggle("line-through")
+              }
+            />
+            <p className="text-white">{`${t}`}</p>
+            <button className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700">
+              Remove
+            </button>
+          </div>
         ))
       )}
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
