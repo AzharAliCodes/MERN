2479db1b770a7b6d8f5271f2020189ac6c9aa90a
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-09 16:01:34 +0530

    complted news section style is reamaing

--- ReactJS/project-19/src/components/News.jsx
+++ ReactJS/project-19/src/components/News.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const News = () => {
   return (
-    <div>
-       News
+    <div className="flex justify-center items-center h-screen bg-slate-800 text-white text-center px-4">
+      <p className="text-lg sm:text-xl font-semibold">
+        📢 This feature is currently not available. <br />
+        We are sorry about it. We will create it soon.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
