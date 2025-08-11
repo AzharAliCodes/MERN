cd24771e8bb26de282bf178bd2dd470b3d87aeea
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-11 12:52:32 +0530

    Updte PlayerName and keep pending it

--- ReactJS/project-19/src/components/PlayerName.jsx
+++ ReactJS/project-19/src/components/PlayerName.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const PlayerName = () => {
   return (
-    <div>
-      PlayerName
+    <div className="flex justify-center items-center h-screen bg-slate-800 text-white text-center px-4">
+      <p className="text-lg sm:text-xl font-semibold">
+        <i className="fi fi-ss-check-circle text-green-500 text-sm"></i>
+        <i className="fi fi-sr-cross-circle text-red-500 text-sm"></i>
+        📢 This feature is currently not available. <br />
+        We are sorry about it. We will create it soon.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default PlayerName
+export default PlayerName;
